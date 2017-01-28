3482d436c7dc11a1db7adddd2837261ce81e708e
Shamikh Hossain <shamikh.hossain@duke.edu>
2017-01-28 11:33:06 -0500

    update

--- firstdjango/urls.py
+++ firstdjango/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from inventory import views
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
     url(r'^item/(?P<id>\d+)/', views.item_detail, name = 'item_detail'),
-    url(r'^admin/', include(admin.site.urls)),
 ]
+
